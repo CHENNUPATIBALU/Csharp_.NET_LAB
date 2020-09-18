b5ee706af087a968f232b12733a7136c3b2df985
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2020-09-18 15:46:37 +0530

    Person_Exercise Project added

---
+++ Person_Exercise/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Exercise
+{
+    class Person
+    {
+        private String firstName;
+        private String lastName;
+        private String emailID;
+        private DateTime dob;
+
+
+        public Person(String fName, String lName, String email, DateTime d)
+        {
+            this.dob = d;
+            this.emailID = email;
+            this.firstName = fName;
+            this.lastName = lName;
+        }
+        public Person(String fName, String lName, String email)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+            this.emailID = email;    
+        }
+        public Person(String fName, String lName, DateTime d)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+            this.dob = d;
+        }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string EmailID { get => emailID; set => emailID = value; }
+        public DateTime Dob { get => dob; set => dob = value; }
+        public bool Adult
+        {
+            get
+            {
+                if ((DateTime.Today.Year - dob.Year) >= 18)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        public String SunSign
+        {
+            get
+            {
+                return new SunSign().ZodaicSign(dob.Year,dob.Month,dob.Day);
+            }
+        }
+        public bool Birthday
+        {
+            get
+            {
+                if ((DateTime.Now.Month == dob.Month) && (DateTime.Now.Day == dob.Day))
+                    return true;
+                else
+                    return false;
+            }
+        }
+        public String ScreenName
+        {
+            get
+            {
+                return lastName + dob.Month.ToString() + SunSign;
+            }
+        }
+
+
+}
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Name: ");
+            String fname = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            String lname = Console.ReadLine();
+            Console.Write("Enter Email: ");
+            String email = Console.ReadLine();
+            if (email.Contains("@")) { }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Email");
+            }
+            Console.WriteLine();
+            Console.WriteLine("** Date Of Birth **");
+            Console.Write("Enter the Birth Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Birth Month: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Birth Date: ");
+            int bdate = Convert.ToInt32(Console.ReadLine());
+
+            DateTime dob = new DateTime(year,month,bdate);
+
+            Person p1 = new Person(fname,lname,email,dob);
+            Console.WriteLine("Person is adult: "+p1.Adult);
+            Console.WriteLine("SunSign: "+p1.SunSign);
+            Console.WriteLine("Today Birthday: "+p1.Birthday);
+            Console.WriteLine("Alloted Screen: "+p1.ScreenName);
+            Console.WriteLine();
+            Person p2 = new Person(fname, lname, email);
+            Console.WriteLine("Person is adult: " + p2.Adult);
+            Console.WriteLine("Today Birthday: " + p2.Birthday);
+            Console.WriteLine("Alloted Screen: " + p2.ScreenName);
+            Console.WriteLine();
+            Person p3 = new Person(fname, lname,  dob);
+            Console.WriteLine("Person is adult: " + p3.Adult);
+            Console.WriteLine("SunSign: " + p3.SunSign);
+            Console.WriteLine("Today Birthday: " + p3.Birthday);
+            Console.WriteLine("Alloted Screen: " + p3.ScreenName);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Person_Exercise/SunSign.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Exercise
+{
+    class SunSign
+    {
+        public String ZodaicSign(int year, int month, int btdate)
+        {
+            if (month == 1 && btdate >= 20 || month == 2 && btdate <= 19)
+            {
+                return "Aquarius";
+            }
+            else if (month == 2 && btdate >= 20 || month == 3 && btdate <= 20)
+            {
+                return "Pisces";
+            }
+            else if (month == 3 && btdate >= 21 || month == 4 && btdate <= 20)
+            {
+                return "Aries";
+            }
+            else if (month == 4 && btdate >= 21 || month == 5 && btdate <= 20)
+            {
+                return "Taurus";
+            }
+            else if (month == 5 && btdate >= 21 || month == 6 && btdate <= 20)
+            {
+                return "Gemini";
+            }
+            else if (month == 6 && btdate >= 21 || month == 7 && btdate <= 22)
+            {
+                return "Cancer";
+            }
+            else if (month == 7 && btdate >= 23 || month == 8 && btdate <= 22)
+            {
+                return "Leo";
+            }
+            else if (month == 8 && btdate >= 23 || month == 9 && btdate <= 22)
+            {
+                return "Virgo";
+            }
+            else if (month == 9 && btdate >= 23 || month == 10 && btdate <= 22)
+            {
+                return "Libra";
+            }
+            else if (month == 10 && btdate >= 23 || month == 11 && btdate <= 22)
+            {
+                return "Scorpio";
+            }
+            else if (month == 11 && btdate >= 23 || month == 12 && btdate <= 21)
+            {
+                return "Sagittarius";
+            }
+            else if (month == 12 && btdate >= 22 || month == 1 && btdate <= 19)
+            {
+                return "Capricorn";
+            }
+            else
+                return "Invald date of birth";
+        }
+    }
+}

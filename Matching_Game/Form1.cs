3f92a497addedaa936c7bedbe02a60a369f8bf7e
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2020-10-16 10:17:56 +0530

    Matching Game project added

---
+++ Matching_Game/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matching_Game
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+        "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

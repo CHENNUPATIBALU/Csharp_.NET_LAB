88180c9f91f70a3c675a08aeac0c799b1407cd8b
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2020-10-16 12:41:58 +0530

    Timer and Icons added

--- Matching_Game/Form1.cs
+++ Matching_Game/Form1.cs
@@ -13,6 +13,8 @@ namespace Matching_Game
     public partial class Form1 : Form
     {
         Random rand = new Random();
+        Label firstClicked = null;
+        Label secondClicked = null;
 
         List<string> icons = new List<string>()
         {
@@ -22,6 +24,39 @@ namespace Matching_Game
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            foreach(Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if(iconLabel!=null)
+                {
+                    int randomNumber = rand.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        private void label_click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    clickedLabel.ForeColor = Color.Black;
+                    return;
+                }
+            }
         }
     }
 }

f5acebb7b31c66cc131ab8adf2b1d9de77dc5080
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2020-10-29 12:46:34 +0530

    FileIODemo project added

---
+++ FileIODemo/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FileIODemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(933, 560);
+            this.textBox1.TabIndex = 0;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(347, 586);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 32);
+            this.openButton.TabIndex = 1;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(516, 586);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 32);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 621);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ FileIODemo/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileIODemo
+{
+    public partial class Form1 : Form
+    {
+        private readonly string fileFilter = @"C:\\Users\\chenn\\Desktop";
+        private readonly string fileType = @"Plain Text Format (.txt)|*.txt|CSharp Source File (.cs)|*.cs";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = null;
+            openFileDialog1.InitialDirectory = fileFilter;
+            openFileDialog1.Filter = fileType;
+            if (reader == null)
+            {
+                try
+                {
+                    if(openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        reader = new StreamReader(openFileDialog1.FileName);
+                        textBox1.Text = reader.ReadToEnd();
+                    }  
+                }
+                catch (FileNotFoundException de)
+                {
+                    MessageBox.Show("Error! File is not found");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex + " Error");
+                }
+                finally
+                {
+                    if (reader != null)
+                        reader.Close();
+                }
+            }
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer = null;
+            saveFileDialog1.InitialDirectory = fileFilter;
+            saveFileDialog1.Filter = fileType;
+            if (writer == null)
+            {
+                try
+                {
+                    if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        writer = new StreamWriter(saveFileDialog1.FileName);
+                        writer.Write(textBox1.Text);
+                    }    
+                }
+                catch (DirectoryNotFoundException de)
+                {
+                    MessageBox.Show("Error! Directory is not found" + "\n" + "Check the directory");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex + " Error");
+                }
+                finally
+                {
+                    if (writer != null)
+                        writer.Close();
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
